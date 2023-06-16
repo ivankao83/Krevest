730cf2983c3716e311e4b5c86a3bf2b83cdc34b7
Ivan <ivankao0803@gmail.com>
2023-06-17 00:55:25 +0800

    Added login & signup page

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Krevest.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+
+        public AccountController(ILogger<AccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+    }
+}
+
--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -24,3 +24,4 @@ namespace Krevest.Controllers
     }
 }
 
+//.
